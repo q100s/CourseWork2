ace1c9aa7df678794d93428f38b8328cff72f074
eduard <eduard42rus@mail.ru>
2023-07-09 20:06:22 +0700

    Init commit

---
+++ src/test/java/pro/sky/java/course2/javaquestionsapp/JavaQuestionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.javaquestionsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaQuestionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
